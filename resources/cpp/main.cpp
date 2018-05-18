d895123c1435209365370f63307124528e047113
Rob <mail@robfors.com>
2018-05-18 01:33:48 -0400

    Fixes esruby.hpp file path

--- resources/cpp/main.cpp
+++ resources/cpp/main.cpp
@@ -1,10 +1,10 @@
-#include "/media/file_server/file_drive/projects/esruby/esruby-esruby/include/esruby.hpp"
+#include "esruby.hpp"
 
 // we need to load the esruby bindings here, see issue:
 //   https://github.com/kripken/emscripten/issues/5537
 // although you can observe that the bindings in esruby-bind do work
 //   so im not sure what the problem is
-// idealy i would like to use a compiler option like EXPORTED_FUNCTIONS, for classes
+// idealy i would like to use a compiler option like EXPORTED_FUNCTIONS, but for classes
 EMSCRIPTEN_BINDINGS(esruby)
 {
   emscripten::class_<ESRuby>("ESRuby")
