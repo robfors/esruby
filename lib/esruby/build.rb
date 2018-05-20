8689e49303798fd68fb4556e13194e6a514c651f
Rob Fors <mail@robfors.com>
2018-05-20 19:02:05 -0400

    Send output files directly to 'output_directory'

--- lib/esruby/build.rb
+++ lib/esruby/build.rb
@@ -143,16 +143,16 @@ module ESRuby
       #args << %q{-s "BINARYEN_METHOD='native-wasm,asmjs'"}
       args << "-s WASM=0"
       args << "-s DISABLE_EXCEPTION_CATCHING=0"
-      RakeFileUtils.sh "emcc --bind #{cxx_include_argument} -o #{build_directory}/#{output_name}.js #{build_directory}/app.o #{build_directory}/main.o #{build_directory}/emscripten/lib/libmruby.a -lm #{js_arguments} #{optimization_argument} #{closure_argument} #{debug_argument} #{flags} -s ALLOW_MEMORY_GROWTH=1 #{args.join(" ")}"
+      RakeFileUtils.sh "emcc --bind #{cxx_include_argument} -o #{output_directory}/#{output_name}.js #{build_directory}/app.o #{build_directory}/main.o #{build_directory}/emscripten/lib/libmruby.a -lm #{js_arguments} #{optimization_argument} #{closure_argument} #{debug_argument} #{flags} -s ALLOW_MEMORY_GROWTH=1 #{args.join(" ")}"
       #if build.build_mode == 'production'
       # ENV["EMCC_CLOSURE_ARGS"] = "--language_in=ECMASCRIPT6" #possibly allow setting output: --language_out=ECMASCRIPT6
       #  sh "java -jar #{PROJECT_DIRECTORY}/emsdk/emscripten/incoming/third_party/closure-compiler/compiler.jar --js #{build.absolute_build_directory}/output.js --js_output_file #{build.absolute_output}"
       #else
         #output_file_extensions = ["asm.js", "js", "js.mem", "wasm"]
-        output_file_extensions = ["js"]
-        output_file_extensions.each do |extension|
-          FileUtils.cp("#{build_directory}/#{output_name}.#{extension}", "#{output_directory}/#{output_name}.#{extension}")
-        end
+        #output_file_extensions = ["js"]
+        #output_file_extensions.each do |extension|
+        #  FileUtils.cp("#{build_directory}/#{output_name}.#{extension}", "#{output_directory}/#{output_name}.#{extension}")
+        #end
       #end
       #--language_in=ECMASCRIPT6
     end
